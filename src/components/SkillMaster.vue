e99106fbfd334325b2a60360c7422b6f14580e8c
BangkitBayu <martinusmartinussaja@gmail.com>
2025-12-06 22:25:56 +0700

    feat: add skill master component in about section

---
+++ src/components/SkillMaster.vue
@@ -0,0 +1,121 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const skills: object = ref([
+  {
+    "name": "HTML",
+    "status": 90
+  },
+  {
+    "name": "CSS",
+    "status": 90
+  },
+  {
+    "name": "Javascript",
+    "status": 70
+  },
+  {
+    "name": "PHP",
+    "status": 70
+  },
+  {
+    "name": "Bootstrap",
+    "status": 70
+  },
+  {
+    "name": "Tailwind",
+    "status": 50
+  },
+  {
+    "name": "Mysql",
+    "status": 70
+  },
+  {
+    "name": "VueJs",
+    "status": 25
+  },
+  {
+    "name": "Laravel",
+    "status": 25
+  },
+  {
+    "name": "ExpressJs",
+    "status": 70
+  },
+  {
+    "name": "Python",
+    "status": 50
+  },
+  {
+    "name": "C#",
+    "status": 50
+  },
+])
+</script>
+<template>
+  <div class="item-box gap-4 w-full place-content-center">
+    <div v-for="({name, status}, index) in skills" :key="index"
+      class="border border-[#8f8f8f] text-center flex items-center justify-center px-3 py-1 rounded-lg bg-[#272727ab] space-x-3">
+      <p class="text-[#8f8f8f] font-semibold text-sm">{{ name }}</p>
+      <div v-if="parseInt(status) >= 90" class=" bg-gray-800 rounded-lg w-full">
+        <div class="bar-90 w-[90%] bg-[#126de4] shadow-[0_0_5px_#126de4] rounded-lg">
+          <p class="text-white text-sm">{{ status }}%</p>
+        </div>
+      </div>
+      <div v-else-if="parseInt(status) >= 70" class=" bg-gray-800 rounded-lg w-full">
+        <div class="bar-70 w-[70%] bg-[#126de4] shadow-[0_0_5px_#126de4] rounded-lg">
+          <p class="text-white text-sm">{{ status }}%</p>
+        </div>
+      </div>
+      <div v-else-if="parseInt(status) >= 50" class=" bg-gray-800 rounded-lg w-full">
+        <div class="bar-50 w-[50%] bg-[#126de4] shadow-[0_0_5px_#126de4] rounded-lg">
+          <p class="text-white text-sm">{{ status }}%</p>
+        </div>
+      </div>
+      <div v-else-if="parseInt(status) >= 25" class=" bg-gray-800 rounded-lg w-full">
+        <div class="bar-25 w-[25%] bg-[#126de4] shadow-[0_0_5px_#126de4] rounded-lg">
+          <p class="text-white text-sm">{{ status }}%</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.item-box {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+}
+
+/* .bar-90 {
+  animation: fill-bar-90 2s ease-in forwards;
+}
+
+.bar-70 {
+  animation: fill-bar-90 2s ease-in forwards;
+}
+
+@keyframes fill-bar-90 {
+  from {
+    width: 0%;
+  }
+
+  to {
+    width: 90%;
+  }
+}
+
+@keyframes fill-bar-70 {
+  from {
+    width: 0%;
+  }
+
+  to {
+    width: 50%;
+  }
+} */
+</style>
+
+
+
+
--- src/components/pages/AboutSection.vue
+++ src/components/pages/AboutSection.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import InfoBox from '../InfoBox.vue';
+import SkillMaster from '../SkillMaster.vue';
 </script>
 <template>
-  <div id="about" class="flex flex-col min-h-screen items-center justify-center space-y-3">
-    <h1 class=" text-white font-bold text-3xl">About Me</h1>
+  <div id="about" class="flex flex-col min-h-screen items-center justify-center space-y-3 mt-36 lg:mt-0">
+    <h1 class=" text-[#126de4] font-bold text-3xl">About Me</h1>
     <div class="p-2 space-y-2">
-      <p class=" text-sm lg:text-xl text-[#a2a2a2] text-center">Hello, I'am Bangkit Bayu Prasetyo, a Software Engineering
+      <p class=" text-lg text-[#a2a2a2] text-center">Hello, I'am Bangkit Bayu Prasetyo, a Software Engineering
         Student was have 1+ years experience in Programming World with strong experince in web development.</p>
-      <p class=" text-sm lg:text-xl text-[#a2a2a2] text-center">In the future, I hope to be Profesional Software Engineer
+      <p class=" text-lg text-[#a2a2a2] text-center">In the future, I hope to be Profesional Software Engineer
         with critical problem solving, good communication and make something impact in tech world.</p>
     </div>
     <div class="item-box gap-4 w-full">
@@ -15,6 +16,9 @@ import InfoBox from '../InfoBox.vue';
       <InfoBox title="2" subtitle="Projects Completed"></InfoBox>
       <InfoBox title="16" subtitle="Years Old"></InfoBox>
     </div>
+    <h1 class=" text-white font-bold text-2xl mt-2">My Skills</h1>
+
+    <SkillMaster></SkillMaster>
   </div>
 </template>
 
