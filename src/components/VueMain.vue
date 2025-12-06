9eb28bca049d7bd136a54abef13b220fea73bcfb
BangkitBayu <martinusmartinussaja@gmail.com>
2025-12-06 22:57:24 +0700

    feat: add projects section page

--- src/components/VueMain.vue
+++ src/components/VueMain.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import HeroSection from './pages/HeroSection.vue';
 import AboutSection from './pages/AboutSection.vue';
+import ProjectsSection from './pages/ProjectsSection.vue';
 </script>
 <template>
   <div class="px-6 bg-transparent">
     <HeroSection></HeroSection>
     <AboutSection></AboutSection>
+    <ProjectsSection></ProjectsSection>
   </div>
 </template>
 
