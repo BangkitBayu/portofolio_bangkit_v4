c43a498d32e554d326f5d093c68f9cbdc07e6175
BangkitBayu <martinusmartinussaja@gmail.com>
2025-12-09 21:58:05 +0700

    feat: add contact page

--- src/components/VueMain.vue
+++ src/components/VueMain.vue
@@ -2,12 +2,14 @@
 import HeroSection from './pages/HeroSection.vue';
 import AboutSection from './pages/AboutSection.vue';
 import ProjectsSection from './pages/ProjectsSection.vue';
+import ContactSection from './pages/ContactSection.vue';
 </script>
 <template>
   <div class="px-6 bg-transparent">
     <HeroSection></HeroSection>
     <AboutSection></AboutSection>
     <ProjectsSection></ProjectsSection>
+    <ContactSection></ContactSection>
   </div>
 </template>
 
---
+++ src/components/pages/ContactSection.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+  import TitlePage from '../TitlePage.vue';
+</script>
+<template>
+  <div id="contact" class="flex flex-col min-h-screen items-center justify-center">
+    <TitlePage title="Contact"></TitlePage>
+  </div>
+</template>
