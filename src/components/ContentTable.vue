f0dda8efe99fe4c780a1a8ecae2efed7624622b7
BangkitBayu <martinusmartinussaja@gmail.com>
2025-12-12 18:10:32 +0700

    feat: add contentTable component

---
+++ src/components/ContentTable.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+  const TableProps: Object = defineProps({
+    headData : Array,
+    rowData: Array
+  })
+</script>
+<template>
+  <div class=" relative border border-[#b5b5b593] rounded-xl overflow-x-auto">
+    <table class="">
+      <thead class=" text-[#cecece] font-extrabold text-left border-b border-[#b5b5b593]">
+        <tr>
+          <th scope="col" class="px-8 py-2">Contact</th>
+          <th scope="col" class="px-8 py-2">Detail</th>
+        </tr>
+      </thead>
+      <tbody class=" text-[#bebebee7] font-medium text-left">
+        <tr>
+          <td scope="row" class="px-8 py-2 font-normal">Location</td>
+          <td scope="row" class="px-8 py-2 font-normal" >Trenggalek, Jawa Timur, Indonesia</td>
+        </tr>
+        <tr>
+          <td scope="row" class="px-8 py-2 font-normal">Email</td>
+          <td scope="row" class="px-8 py-2 font-normal" ><a href="mailto:martinusmartinussaja@gmail.com">martinusmartinussaja@gmail.com</a></td>
+        </tr>
+        <tr>
+          <td scope="row" class="px-8 py-2 font-normal">Location</td>
+          <td scope="row" class="px-8 py-2 font-normal" >Trenggalek, Jawa Timur, Indonesia</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
--- src/components/pages/ContactSection.vue
+++ src/components/pages/ContactSection.vue
@@ -1,8 +1,25 @@
 <script setup lang="ts">
-  import TitlePage from '../TitlePage.vue';
+import TitlePage from '../TitlePage.vue';
+import ContentTable from '../ContentTable.vue';
+
+const ContactTableData: Object = {
+  headData: ["Contact", "Detail"],
+  bodyData: {
+    rowFirst: ["Location", "Trenggalek, Jawa Timur, Indonesia"],
+    rowSecond: ["Email", "martinusmartinussaja@gmail.com"],
+    rowThird: ["Phone", "+62 858-1338-5224"]
+  }
+}
 </script>
 <template>
-  <div id="contact" class="flex flex-col min-h-screen items-center justify-center">
-    <TitlePage title="Contact"></TitlePage>
+  <div id="contact" class=" flex flex-col min-h-screen items-center justify-center space-y-4">
+    <TitlePage title="Contact Me"></TitlePage>
+    <div class="place-content-center place-items-center w-full"
+      style="display: grid; grid-template-columns: repeat(auto-fill, minmax(500px , 1fr));">
+      <div class=" flex flex-col space-y-3">
+        <h3 class=" text-white font-bold text-2xl">Let's Connect!</h3>
+        <ContentTable></ContentTable>
+      </div>
+    </div>
   </div>
 </template>
