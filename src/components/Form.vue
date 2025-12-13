8289c212d3b71e5d3edcfa1768e9f1a52461e132
BangkitBayu <martinusmartinussaja@gmail.com>
2025-12-13 23:10:13 +0700

    fix: send email logic in send message form at contact section

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import useVuelidate from "@vuelidate/core"
 import { email, helpers, required } from "@vuelidate/validators"
-import { computed, reactive} from "vue"
+import { computed, reactive } from "vue"
 
 interface Message {
   fullName: string
@@ -36,24 +36,20 @@ const v$ = useVuelidate(rules, newMessage, { $externalResults: {} })
 async function sendMessage() {
   const isFormValid = await v$.value.$validate
   if (isFormValid) {
-    alert("Success")
+    console.log("Success")
   }
-  // alert("Failed")
-  // const data = { ...newMessage.values }
-  // const { fullName, from, to, subject, text } = data
-
-  // alert(`${fullName} - ${from} - ${to} - ${subject} - ${text}`)
 }
 </script>
 <template>
   <div>
-    <form method="POST" class="border border-third rounded-lg p-4 mt-4 flex flex-col space-y-3 w-full">
+    <form action="https://formspree.io/f/xpwvkzaz" method="POST"
+      class="border border-third rounded-lg p-4 mt-4 flex flex-col space-y-3 w-full" autocomplete="on">
       <div class=" flex w-full justify-between space-x-3">
         <div class="form-group flex flex-col text-left space-y-2 w-[50%]">
           <label class=" font-semibold text-primary text-sm">
             Your full name:
           </label>
-          <input type="text" name="fullName" class="text-sm border border-third text-third-50 rounded-sm px-2 py-2 w-full"
+          <input type="text" name="Fullname" class="text-sm border border-third text-third-50 rounded-sm px-2 py-2 w-full"
             placeholder=" Enter full name here" required v-model="newMessage.fullName" @blur="v$.fullName.$touch">
           <div v-if="v$.fullName.$error">
             <span v-for="error of v$.fullName.$errors" :key="error.$uid"
@@ -66,7 +62,7 @@ async function sendMessage() {
           <label class=" font-semibold text-primary text-sm">
             Your email:
           </label>
-          <input type="email" name="email" class="text-sm border border-third text-third-50 rounded-sm px-2 py-2 w-full"
+          <input type="email" name="Email" class="text-sm border border-third text-third-50 rounded-sm px-2 py-2 w-full"
             placeholder=" Enter email here" required v-model="newMessage.to" @blur="v$.to.$touch">
           <div v-if="v$.to.$error">
             <span v-for="error of v$.to.$errors" :key="error.$uid" class=" text-red-500 transition delay-150 ease-in">
@@ -79,7 +75,7 @@ async function sendMessage() {
         <label class=" font-semibold text-primary text-sm">
           Your subject:
         </label>
-        <input type="text" name="subject" class="text-sm border border-third text-third-50 rounded-sm px-2 py-2 w-full"
+        <input type="text" name="Subject" class="text-sm border border-third text-third-50 rounded-sm px-2 py-2 w-full"
           placeholder="What it's about.." required v-model="newMessage.subject" @blur="v$.subject.$touch">
         <div v-if="v$.subject.$error">
           <span v-for="error of v$.subject.$errors" :key="error.$uid" class=" text-red-500 transition delay-150 ease-in">
@@ -91,7 +87,7 @@ async function sendMessage() {
         <label class=" font-semibold text-primary text-sm">
           Your message:
         </label>
-        <textarea name="message"
+        <textarea name="Message"
           class="text-sm resize-none border border-third text-third-50 rounded-sm px-2 py-2 w-full min-h-20"
           placeholder="You can just say Hello or tell your project idea maybe i can help you." required
           v-model="newMessage.text" @blur="v$.text.$touch"></textarea>
@@ -101,8 +97,8 @@ async function sendMessage() {
           </span>
         </div>
       </div>
-      <button @click.prevent="sendMessage" :disabled="v$.$invalid"
-        class=" w-full p-2 bg-brand rounded-sm text-primary">Send Message</button>
+      <button v-on:submit="sendMessage" :disabled="v$.$invalid" class=" w-full p-2 bg-brand rounded-sm text-primary">Send
+        Message</button>
     </form>
   </div>
 </template>
